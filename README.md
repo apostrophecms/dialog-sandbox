3439c7196ec1f5c6821a5c406e4327f1cb9061ff
Stuart Romanek <stuart@punkave.com>
2019-10-18 17:39:50 -0400

    initial

---
+++ README.md
@@ -0,0 +1,12 @@
+# Dialog sandbox
+
+## Getting started locally
+- Clone this repo and https://github.com/apostrophecms/apostrophe-dialog-box into separate directories
+- Run `npm link` inside the `apostrophe-dialog-box` directory
+- In the sandbox directory run
+  -- `npm i`
+  -- `npm node app.js apostrophe-users:add USERNAME admin
+    -- fill in password when prompted
+  - `npm link apostrophe-dialog-box`
+
+`npm start` will run the app
---
+++ app.js
@@ -0,0 +1,23 @@
+var path = require('path');
+
+var apos = require('apostrophe')({
+  shortName: 'dialogSandbox',
+  bundles: [ 'apostrophe-dialog-box' ],
+
+  modules: {
+    'apostrophe-templates': { viewsFolderFallback: path.join(__dirname, 'views') },
+    'apostrophe-dialog-box': {
+      addFields: [
+        {
+          name: 'template',
+          label: 'Template',
+          type: 'select',
+          choices: [
+            { label: 'Default', value: 'default' },
+            { label: 'Wacky', value: 'wacky' }
+          ]
+        }
+      ]
+    }
+  }
+});
